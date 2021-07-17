f34f31873da76ddc0ea731987c8e015ae6498512
Won <mynameiswon@hotmail.com>
2021-07-17 15:43:10 +1000

    addition and subtraction completed

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import random
+import itertools
 
 
 # configuration
@@ -13,10 +15,51 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+
+#firstNum = [1,2,3,4,5,6,7,8,9,10,11,12]
+#secondNum = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+#pairs = list(itertools.product(firstNum,secondNum))
+#random.shuffle(pairs)
+
+#for x,y in pairs:
+    #print(f'{x} x {y} = ')
+
+#QUESTION: 3 + 2
+#CORRECT ANSWER: 5
+#INCORRECT ANSWER: 4, 3, 6
+
+def subtract():
+    num1 = random.randint(2, 100)
+    num2 = random.randint(1, num2 - 1)
+    question = str(num1) + " - " + str(num2)
+    correctAnswer = num1 - num2
+    incorrectAnswer1 = correctAnswer - 1
+    incorrectAnswer2 = correctAnswer + 1
+    if incorrectAnswer1 != 0:
+        incorrectAnswer3 = incorrectAnswer1 - 1
+    else:
+        incorrectAnswer3 = incorrectAnswer2 + 1
+    newDict = {
+        "question": question, 
+        "correctAnswer": correctAnswer,
+        "incorrectAnswer": [incorrectAnswer1, incorrectAnswer2, incorrectAnswer3],
+    }
+
+
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/battle', methods=['GET', 'POST'])
+def battle():
+    #for loop of 10 questions
+    #randomised between addition, subtraction, multiplication, division
+    #subtract
+    num1 = [x for x in range(1, 100)]
+    return jsonify(num1)
+
 
 
 if __name__ == '__main__':
